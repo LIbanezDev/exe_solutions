cb0d45f0140634d55082f1a100567d6e4da697be
Lucas Vergara Ibañez <lucas.ivergara18@gmail.com>
2019-11-15 14:44:43 -0300

    f

--- Avanzada 2019/Videojuego.cpp
+++ Avanzada 2019/Videojuego.cpp
@@ -28,27 +28,25 @@ class Ataque{
 };
 class Heroe{		
 	protected:
-		int ataque;
 		int p_ataque;
 		int vida;
 		string nombre;
 		int armor;
-		Ataque *ataques[3];
+		Ataque *ataques[4];
 	public:
-		Heroe(int vida, string nombre, int ataque, int armor, int p_ataque){
-			this->ataque  = ataque;
+		Heroe(int vida, string nombre, int armor, int p_ataque){
 			this->p_ataque = p_ataque;
 			this->vida = vida;
 			this->nombre = nombre;
 			this->armor = armor;
 		}
-		void setAtaque(int ataque){
-			this->ataque = ataque;
+		void setP_ataque(int p){
+			this->p_ataque = p;
 		}
-		int getAtaque(){
-			return ataque;
+		int getP_ataque(){
+			return p_ataque;
 		}
-				void setVida(int vida){
+		void setVida(int vida){
 			this->vida = vida;
 		}
 		void setNombre(string nombre){
@@ -67,7 +65,7 @@ class Heroe{
 			return armor;
 		}
 		void agregarHechizo(int danho, int costo){
-			for(int i = 0; i < 3; i++){
+			for(int i = 0; i < 4; i++){
 				if(ataques[i] == NULL){
 					ataques[i]->setDanho(danho);
 					ataques[i]->setCosto(costo);
@@ -81,7 +79,7 @@ class Enemigo: public Heroe{
 	private:
 		Ataque *ataquesx[3];
 	public:
-		Enemigo(int vida, string nombre, int ataque, int armor, int p_ataque):Heroe(vida, nombre, ataque, armor, p_ataque){
+		Enemigo(int vida, string nombre, int armor, int p_ataque):Heroe(vida, nombre, armor, p_ataque){
 		}
 	void agregarHechizo(int danho, int costo){
 			for(int i = 0; i < 3; i++){
@@ -117,19 +115,6 @@ class Juego{
 				cout<<"Ya hay un heroe en el juego!"<<endl;
 			}			
 		}
-		void atacarEnemigo(string nombreEnemigo){
-			for(int i = 0; i < 4; i++){
-				if(enemigos[i]->getNombre() == nombreEnemigo){
-					if(heroe != NULL){
-					cout<<"El heroe a atacado al enemigo: "<<nombreEnemigo<<"!!"<<endl;
-					enemigos[i]->setVida(enemigos[i]->getVida() - heroe->getAtaque());
-					cout<<"La vida del enemigo quedo en "<<enemigos[i]->getVida()<<endl;
-					}else{
-						break;
-					}
-				}
-			}
-		}
 };
 int main(){
 	string n;
@@ -142,28 +127,30 @@ int main(){
 	cout<<"			Ingresa tu nombre de paco: ";
 	cin>>n;
 	cout<<endl;
-	Heroe *heroe = new Heroe(1000, n, 25, 65, 20);
+	Heroe *heroe = new Heroe(1000, n, 65, 20);
 	cout<<" "<<heroe->getNombre()<<", tus estadisticas son las siguientes:"<<endl<<endl;
 	cout<<" "<<heroe->getVida()<<" de vida"<<endl;
-	cout<<" "<<heroe->getAtaque()<<" de ataque"<<endl;
 	cout<<" "<<heroe->getArmor()<<" de armadura"<<endl<<endl;
 	cout<<" "<<"Estos son tus enemigos! "<<endl<<endl;
-	Enemigo *e1 = new Enemigo(300, "mapuche", 15, 60, 20);
-	Enemigo *e2 = new Enemigo(200, "liceano", 45, 20,20);
-	Enemigo *e3 = new Enemigo(150, "negro matapacos", 25, 35,20);
-	Enemigo *e4 = new Enemigo(320, "vendedor de verduras", 30, 38,20);
+	Enemigo *e1 = new Enemigo(300, "mapuche", 60, 20);
+	Enemigo *e2 = new Enemigo(200, "liceano", 20,20);
+	Enemigo *e3 = new Enemigo(150, "negro matapacos", 35,20);
+	Enemigo *e4 = new Enemigo(320, "vendedor de verduras", 38,20);
 	cout<<" Nombre:             "<<e1->getNombre()<<"   |   "<<e2->getNombre()<<"  |   "<<e3->getNombre()<<"  |  "<<e4->getNombre()<<endl;
 	cout<<" Vida:                "<<e1->getVida()<<"      |     "<<e2->getVida()<<"    |        "<<e3->getVida()<<"         |        "<<e4->getVida()<<endl;
-	cout<<" Ataque:               "<<e1->getAtaque()<<"      |      "<<e2->getAtaque()<<"    |         "<<e3->getAtaque()<<"         |         "<<e4->getAtaque()<<endl;
 	cout<<" Armadura:             "<<e1->getArmor()<<"      |      "<<e2->getArmor()<<"    |         "<<e3->getArmor()<<"         |         "<<e4->getArmor()<<endl<<endl<<endl;
 
 	
 	cout<<"                                COMIENZA EL JUEGO"<<endl;
-	cout<<" "<<"REGLAS: "<<endl;
+	cout<<" "<<"REGLAS"<<endl<<endl;
+	cout<<" -"<<"Tanto tu como tus enemigos cuentan con 20 puntos de ataque al inicio del turno"<<endl;
+	cout<<" -"<<"En cada turno deberas usar tus puntos de ataque disponibles."<<endl;
+	cout<<" -"<<"Como paco, cuentas con 4 hechizos disponibles, los cuales consumen puntos de ataque."<<endl;
+	cout<<" -"<<"A traves de "<<endl;
 	
 	while(true){
-		cout<<"Es tu turno de atacar."<<endl<<endl;
-		cout<<"Tienes: ";
+		cout<<" Es tu turno de atacar,"<<" tienes "<<heroe->getP_ataque()<<" puntos de ataque."<<endl;
+		
 		break;
 		
 		
