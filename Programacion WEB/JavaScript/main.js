011d08f11509c50be4b19e2e71fd1fe5fb3d0e47
Lucas Vergara Ibañez <lucas.ivergara18@gmail.com>
2019-12-01 22:04:19 -0300

    Avance proyecto html js css

---
+++ Programacion WEB/JavaScript/main.js
@@ -0,0 +1,10 @@
+var titulo = document.getElementById("titulo");
+var parrafos = document.getElementsByClassName("parrafo");
+var boton = document.getElementById("boton");
+
+
+
+boton.addEventListener("click", function(){
+    titulo.style.color = "red";
+})
+
