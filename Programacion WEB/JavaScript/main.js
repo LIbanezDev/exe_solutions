e2fae45dbf18e6d9006916d509221348b3c870bd
Lucas Vergara Ibañez <lucas.ivergara18@gmail.com>
2019-12-04 04:04:51 -0300

    ...

--- Programacion WEB/JavaScript/main.js
+++ Programacion WEB/JavaScript/main.js
@@ -1,10 +1,7 @@
+
+
 var titulo = document.getElementById("titulo");
-var parrafos = document.getElementsByClassName("parrafo");
-var boton = document.getElementById("boton");
 
-
-
-boton.addEventListener("click", function(){
-    titulo.style.color = "red";
-})
-
+function cambiarTitulo(){
+    titulo.innerHTML = "Nuevo titulo!";
+}
