384fab6d5f443c086f27ecb3a030643427db9ee2
Lucas Vergara Ibañez <lucas.ivergara18@gmail.com>
2020-01-05 20:28:25 -0300

    Java con JOptionPane

--- Javita/src/main/java/Herencia/Main.java
+++ Javita/src/main/java/Herencia/Main.java
@@ -1,10 +1,19 @@
 package Herencia;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String [] args){
-        Scanner entrada = new Scanner(System.in);
+        int xd;
+        float f;
+        String cadena = JOptionPane.showInputDialog("Ingrese cadena de caracteres.");
+        xd = Integer.parseInt(JOptionPane.showInputDialog("Ingrese entero"));
+        f = Float.parseFloat(JOptionPane.showInputDialog("Ingrese float"));
+        JOptionPane.showMessageDialog(null, "La cadena es: " + cadena);
+        JOptionPane.showConfirmDialog(null, "viejo lesbiano");
+        System.out.println("Datos ingresados: " + cadena + " " + xd + " " + f);
+                
          
     }
 }
