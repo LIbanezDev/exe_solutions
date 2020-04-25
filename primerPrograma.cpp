70547dd2c24c4696be66cf0f6a531c99ecc81346
Lucas Ignacio <lucas.vergara@usm.cl>
2020-04-25 16:03:52 -0400

    del baúl de los recuerdos

---
+++ primerPrograma.cpp
@@ -0,0 +1,33 @@
+//Primer problema resuelto en lenguaje C++
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+	
+  float A, B, C, D, Resultado=0;
+  
+  cout<<"Digite A:"; cin>>A;
+   cout<<"Digite B:"; cin>>B;
+    cout<<"Digite C:"; cin>>C;
+     cout<<"Digite D:"; cin>>D;
+     
+    Resultado = (A+B) / (C+D);
+    cout.precision(3);
+    cout<<"El resultado de la operacion (a+b):(c+d) es:"<<Resultado<<endl;
+
+return 0;
+}
+
+
+
+
+
+
+
+
+	
+
